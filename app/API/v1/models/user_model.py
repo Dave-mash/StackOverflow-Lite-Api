4a0a07758ec1f6a00301837ad914a714595666b4
Dave-mash <macharia3041@gmail.com>
2018-12-30 15:40:17 +0300

    [chore #162826325] Added edit and delete account tests

--- app/API/v1/models/user_model.py
+++ app/API/v1/models/user_model.py
@@ -1,17 +1,16 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class UserModel:
     """ add user a user to a database """
 
-    def __init__(self, logged=False):
-        self.logged = logged,
+    def __init__(self):
         self.dup_email = None,
         self.dup_username = None,
         self.db = []
 
     def create_account(self, payload):
-        generate_password_hash(payload['password'])
         payload["id"] = len(self.db)
+        payload['password'] = generate_password_hash(payload['password'])
         
         # Check for duplicate email and username
         self.dup_email = [users for users in self.db if users['email'] == payload['email']]
@@ -31,13 +30,23 @@ class UserModel:
             self.db.append(payload)
             return self.db
 
-    def edit_account(self, id, updates=None, logged=False):
+    def get_user(self, email, password):
+        user = [user for user in self.db if email == user['email']]
+
+        if user:
+            if check_password_hash(user[0]['password'], password):
+                return 'SUCCESS'
+            else:
+                return 'INVALID'
+        else:
+            return 'FAILURE'
+
+    def edit_account(self, id, updates=None):
         from app.API.v1.views.user_views import user_model as registered_user
-        if logged:    
-            for i in range(len(registered_user.db)):
-                if registered_user.db[i]["id"] == id:
-                    registered_user.db[i] = updates
-                    return registered_user.db
+        for i in range(len(registered_user.db)):
+            if registered_user.db[i]["id"] == id:
+                registered_user.db[i] = updates
+                return registered_user.db
 
     def del_account(self, id):
         user = [user for user in self.db if user['id'] == id]
